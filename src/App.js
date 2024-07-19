871963a3e442da2cada4ea7969bdefdd9150a93c
PEDRO HENRIQUE DE BORTOLI <03298022080@senacrs.edu.br>
2024-07-19 12:21:11 -0300

    fazendo lista

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
 import Lista from './components/listaCompetencias'
 
 function App() {
-  const tasks = [];
   return (
     <div className="App">
-      <Lista content={tasks}/>
+      <Lista />
     </div>
   );
 }
--- src/components/listaCompetencias.jsx
+++ src/components/listaCompetencias.jsx
@@ -1,16 +1,35 @@
-function Lista ({content}) {
+import React, { useState } from 'react';
+import '../style.css';
+
+function Lista() {
+    const [tasks, setTasks] = useState([]);
+    const [novaTarefa, setNovaTarefa] = useState('');
+
+    const handleAddTask = () => {
+        const newTask = { id: tasks.length + 1, text: novaTarefa };
+        setTasks([...tasks, newTask]);
+        setNovaTarefa('');
+    };
 
     return (
         <div>
-            <h2>Você tem estes componentes a fazer:</h2>
+            <h2>Você tem estas tarefas a fazer:</h2>
             <ul>
-                {content.map(task => (
-                <li key={task.id}>{task.text}</li>
-            ))}
+                {tasks.map(task => (
+                    <li key={task.id}>{task.text}</li>
+                ))}
             </ul>
             
+            <input
+                type="text"
+                id="novaTarefa"
+                placeholder="Nova Tarefa..."
+                value={novaTarefa}
+                onChange={(e) => setNovaTarefa(e.target.value)}
+            />
+            <button onClick={handleAddTask}>Adicionar Tarefa</button>
         </div>
     );
-};
+}
 
 export default Lista;
